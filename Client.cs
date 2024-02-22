bebec40fdcb7d84ae807174ba599df171107f920
ai-dev-official <ai.codespro@gmail.com>
2024-02-22 17:17:51 +0000

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PhoneBookClient
+{
+    class Client
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly String baseURI = "http://localhost:2737/api/phonebook/";
+
+        static async Task RunAsync()
+        {
+            try
+            {
+                // GET /api/phonebook/all
+                var serializer = new DataContractJsonSerializer(typeof(List<PhoneBookEntry>));
+
+                var streamTask = client.GetStreamAsync(baseURI + "all");
+
+                var entries = serializer.ReadObject(await streamTask) as List<PhoneBookEntry>;
+
+                foreach( var e in entries)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                // GET /api/phonebook/name/{name}
+
+                streamTask = client.GetStreamAsync(baseURI + "name/Joe Soap");
+
+                entries = serializer.ReadObject(await streamTask) as List<PhoneBookEntry>;
+
+                foreach(var e in entries)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+
+                // GET /api/phonebook/number/{number}
+                serializer = new DataContractJsonSerializer(typeof(PhoneBookEntry));
+
+                streamTask = client.GetStreamAsync(baseURI + "number/0239305383");
+
+                var entry = serializer.ReadObject(await streamTask) as PhoneBookEntry;
+
+                Console.WriteLine(entry);
+        
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+        // Kick off
+        static void Main()
+        {
+            RunAsync();
+            client.Dispose();
+            Console.ReadLine();
+        }
+    
+    }
+}
---
+++ PhoneBookEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace PhoneBookClient
+{
+    [DataContract]
+    public class PhoneBookEntry
+    {
+        [DataMember(Name = "number")]                   // camelCasing
+        public string Number { get; set; }              // unique
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "address")]
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " " + Address + " " + Number;
+        }
+    }
+}
+
